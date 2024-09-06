67a5188e63b16d0eb6fe0f6bb180567c75a54851
Lucas Serrano Rocco <156000513+lucasserrano10@users.noreply.github.com>
2024-09-06 20:04:33 -0300

    fix: 'modificacoes navbar'

--- checkpoint/src/componentes/Nav/Nav.jsx
+++ checkpoint/src/componentes/Nav/Nav.jsx
@@ -7,22 +7,20 @@ import Navbar from 'react-bootstrap/Navbar';
 
 const Menu = () => {
     return (
-        <div>
-            <Navbar expand="lg" className="bg-body-tertiary">
-            <Container>
-                <Navbar.Brand href="#logo">Power Run</Navbar.Brand>
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container fluid className='background-nav p-4'>
+                <Navbar.Brand href="#logo" className='fs-2 fw-bold text-shadow text-white'>Power Run</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ms-auto gap-5">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#sobre">Sobre</Nav.Link>
-                    <Nav.Link href="#produtos">Produtos</Nav.Link>
-                    <Nav.Link href="#contato">Contato</Nav.Link>
+                <Nav className="ms-auto gap-2">
+                    <Nav.Link href="#home" className='text-white text-shadow-itens fs-4 '>Home</Nav.Link>
+                    <Nav.Link href="#sobre" className='text-white text-shadow-itens fs-4 '>Sobre</Nav.Link>
+                    <Nav.Link href="#produtos" className='text-white text-shadow-itens fs-4 '>Produtos</Nav.Link>
+                    <Nav.Link href="#contato" className='text-white text-shadow-itens fs-4 '>Contato</Nav.Link>
                 </Nav>
                 </Navbar.Collapse>
             </Container>
-            </Navbar>
-        </div>
+        </Navbar>
     );
 };
 
